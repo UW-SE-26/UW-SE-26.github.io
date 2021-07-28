6e0656045d1a31a178e9633659c2f5c229d99837
George Shao <GeorgeShao123@gmail.com>
2021-07-27 20:26:51 -0400

    Added example interactive crosshairs/hints

--- src/charts/ExampleBarChart.js
+++ src/charts/ExampleBarChart.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   XYPlot,
   XAxis,
   YAxis,
   VerticalGridLines,
   HorizontalGridLines,
+  Crosshair,
   VerticalBarSeries,
 } from "react-vis";
 
-function ExampleChart(props) {
+function ExampleBarChart(props) {
+  const [crosshairValues, setCrosshairValues] = useState([]);
+
   return (
     <div>
-      <XYPlot height={300} width={300} xType="ordinal">
+      <XYPlot
+        height={300}
+        width={300}
+        xType="ordinal"
+        onMouseLeave={() => setCrosshairValues([])}
+      >
         <VerticalGridLines />
         <HorizontalGridLines />
         <XAxis />
         <YAxis />
-        <VerticalBarSeries data={props.data} />
+        <VerticalBarSeries data={props.data} onNearestX={(value, {index}) => setCrosshairValues([{"x": props.data.map(d => d["x"])[index], "y": props.data.map(d => d["y"])[index]}])} />
+        <Crosshair values={crosshairValues} />
       </XYPlot>
     </div>
   );
 }
 
-export default ExampleChart;
+export default ExampleBarChart;
--- src/charts/ExampleLineChart.js
+++ src/charts/ExampleLineChart.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   XYPlot,
   XAxis,
   YAxis,
   VerticalGridLines,
   HorizontalGridLines,
+  Crosshair,
   LineSeries,
 } from "react-vis";
 
-function ExampleChart(props) {
+function ExampleLineChart(props) {
+  const [crosshairValues, setCrosshairValues] = useState([]);
+
   return (
     <div>
-      <XYPlot height={300} width={300}>
+      <XYPlot height={300} width={300} onMouseLeave={() => setCrosshairValues([])}>
         <VerticalGridLines />
         <HorizontalGridLines />
         <XAxis />
         <YAxis />
-        <LineSeries data={props.data} />
+        <LineSeries data={props.data} onNearestX={(value, {index}) => setCrosshairValues([{"x": props.data.map(d => d["x"])[index], "y": props.data.map(d => d["y"])[index]}])} />
+        <Crosshair values={crosshairValues} />
       </XYPlot>
     </div>
   );
 }
 
-export default ExampleChart;
+export default ExampleLineChart;
