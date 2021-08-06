1a8fa7e983af3d9e1fae9ca27f534ea97439ba4f
George Shao <GeorgeShao123@gmail.com>
2021-08-06 11:10:49 -0400

    Added multi bar chart

--- src/data.js
+++ src/data.js
@@ -19,4 +19,8 @@ const data_example_bar = [
   { x: "2021", y: 15 },
 ];
 
-export { data_example_line, data_example_bar };
+const data_example_multibar = [{x: 1, y: 10}, {x: 2, y: 5}, {x: 3, y: 15}];
+
+const data_example_multibar2 = [{x: 1, y: 12}, {x: 2, y: 2}, {x: 3, y: 11}];
+
+export { data_example_line, data_example_bar, data_example_multibar, data_example_multibar2 };
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ExampleBarChart from "../charts/ExampleBarChart"
 import ExampleLineChart from "../charts/ExampleLineChart"
-import { data_example_bar, data_example_line } from "../data"
+import ExampleMultiBarChart from "../charts/ExampleMultiBarChart"
+import { data_example_bar, data_example_line, data_example_multibar, data_example_multibar2 } from "../data"
 
 function MainPage() {
   return (
@@ -9,6 +10,7 @@ function MainPage() {
       <p>UW SE 26 Class Profile</p>
       <ExampleBarChart data={data_example_bar} />
       <ExampleLineChart data={data_example_line} />
+      <ExampleMultiBarChart data={data_example_multibar} data2={data_example_multibar2}/>
     </div>
   );
 }
