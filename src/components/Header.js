e2b5d77f6a2aa0f4668538eb86fc0f972e364ac3
Henry Stelle <21stellehenry@bprep.org>
2021-10-28 12:45:08 -0400

    use react-router-dom correctly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../styling/Header.css';
 
-function Header(props) {
-
-    const activeClass = (page) => page === props.activePage && 'active';
-
+function Header() {
     return (
-        <div>
-            <header className={'main_header'}>
-                <h3>SE 2026 Profile</h3>
-                <div class={'spacer'} />
-                <button onClick={props.setAbout} className={activeClass('about')}>About</button>
-                <button onClick={props.setDemographics} className={activeClass('demographics')}>Demographics</button>
-                <button onClick={props.setAcademics} className={activeClass('academics')}>Academics</button>
-                <button onClick={props.setHousing} className={activeClass('housing')}>Housing</button>
-                <button onClick={props.setSexDrugs} className={activeClass('sexDrugs')}>Sex/Drugs</button>
-                <button onClick={props.setMisc} className={activeClass('misc')}> Misc</button>
-            </header>
-        </div>
+        <header className={'main_header'}>
+            <h3>SE 2026 Profile</h3>
+            <div class={'spacer'} />
+            {['About', 'Demographics', 'Academics', 'Housing', 'Sex/Drugs', 'Misc'].map((page, i) => 
+                <NavLink
+                    to={`/class-profile/${page.toLowerCase().replace('/', '-')}`}
+                    activeClassName={'active'}
+                    className={'link'}
+                >
+                    {page}
+                </NavLink>
+            )}
+        </header>
     )
 }
 
